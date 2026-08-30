faf7fe1b91e427428c506ed0ae64f1a12a8462a9
pauladuarte13-ctrl <pauladuarte13@gmail.com>
2026-08-30 22:52:14 +0100

    Update build.gradle.kts

--- CoposColecao_GitHub_APK/CoposColecao/app/build.gradle.kts
+++ CoposColecao_GitHub_APK/CoposColecao/app/build.gradle.kts
@@ -21,7 +21,14 @@ android {
         compose = true
         buildConfig = true
     }
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
+kotlinOptions {
+    jvmTarget = "17"
+}
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
